e569e4a0bafe7b0e219acb93dd8a97be6a069f95
jlegutko <spraite111@gmail.com>
2023-01-10 13:38:27 +0100

    Working in T1 with rank other than 1

--- MPIEnigmaBreaker.cpp
+++ MPIEnigmaBreaker.cpp
@@ -55,10 +55,17 @@ void MPIEnigmaBreaker::crackMessage() {
 	 * Umawiamy się niniejszym, że więcej niż maxRotors rotorów nie będzie!
 	 */
 	MPI_Request endOfComputing;
+	MPI_Request broadcastResult;
+	MPI_Request finderRank;
 	int *dummy = new int[1];
+	int *rankr = new int[1];
+	int *rankl = new int[1];
+	rankl[0] = rank;
 	dummy[0] = 0;
 	int gotoCheck =0;
-
+	int endCheck = 0;
+	if (rank == 0)
+	rankr[0] = -1;
 	uint *rMax = new uint[ MAX_ROTORS ];
 	for ( uint rotor = 0; rotor < MAX_ROTORS; rotor++ ) {
 		if ( rotor < rotors )
@@ -68,19 +75,12 @@ void MPIEnigmaBreaker::crackMessage() {
 	}
 
 	uint *r = new uint[ MAX_ROTORS ];
+
 rMax[0] = ((rotorLargestSetting * (rank + 1)) / size) < rotorLargestSetting ? ((rotorLargestSetting * (rank + 1)) / size) : rotorLargestSetting;
 		//cout << "JESTEM RANK ustawiłem rMAx na " << rank << rMax[0] << endl;
 	for (r[0] = (rotorLargestSetting * rank) / size;r[0] <= rMax[0]; r[0]++ ){
-							if (rank !=0){
-					MPI_Ibcast(dummy,1,MPI_INT,0,MPI_COMM_WORLD,&endOfComputing);
-					//cout << "Gotocheck ma " << gotoCheck << endl;
-					//MPI_Test(&endOfComputing,&gotoCheck,MPI_STATUS_IGNORE);
-					//cout << "Gotocheck ma " << gotoCheck << endl;
-					if (dummy[0]) {
-						//cout << "Odebralem ibcast " << rank << endl;
-						goto EXIT_ALL_LOOPS;
-					}
-				}
+
+				
 		for (r[1]=0;r[1] <= rMax[1]; r[1]++) {
 
 			for (r[2]=0;r[2] <= rMax[2]; r[2]++)
@@ -91,19 +91,25 @@ rMax[0] = ((rotorLargestSetting * (rank + 1)) / size) < rotorLargestSetting ? ((
 								for (r[7]=0;r[7] <= rMax[7]; r[7]++)
 									for (r[8]=0;r[8] <= rMax[8]; r[8]++)
 										for (r[9]=0; r[9] <= rMax[9]; r[9]++) {
+											if (rank == 6 || rank == 1)
+												cout << "proces " << rank << endl;
+											MPI_Ibcast(dummy,1,MPI_INT,0,MPI_COMM_WORLD,&endOfComputing);
+											MPI_Test(&endOfComputing,&endCheck,MPI_STATUS_IGNORE);
+											if (dummy[0]) {
+												cout << "Odebralem ibcast " << rank << endl;
+												goto EXIT_ALL_LOOPS;
+											}
+											if (rank == 0) {
+												MPI_Irecv(rankr,1,MPI_INT,MPI_ANY_SOURCE,3,MPI_COMM_WORLD,&finderRank);
+												MPI_Test(&finderRank,&gotoCheck,MPI_STATUS_IGNORE);
+												if (rankr[0] != -1)
+													goto EXIT_ALL_LOOPS;
+											}
 											if ( solutionFound( r ) ) {
-											/*	cout <<"Ja proces " << rank<< " Znalazłem rozwiązanie Wysyłam do procesu 0 coś takiego " << r[0] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[1] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[2] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[3] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[4] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[5] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[6] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[7] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[8] << endl;
-												cout << "Wysyłam do procesu 0 coś takiego " << r[9] << endl;
-												*/
-												MPI_Send(r,MAX_ROTORS,MPI_INT,0,0,MPI_COMM_WORLD);
+												cout <<"Ja proces " << rank<< " Znalazłem rozwiązanie Wysyłam do procesu 0 coś takiego " << r[0] << endl;
+												
+												MPI_Isend(rankl,1,MPI_INT,0,3,MPI_COMM_WORLD,&finderRank);
+												MPI_Send(r,MAX_ROTORS,MPI_INT,0,2,MPI_COMM_WORLD);
 												goto EXIT_ALL_LOOPS;
 											}
 												
@@ -111,22 +117,13 @@ rMax[0] = ((rotorLargestSetting * (rank + 1)) / size) < rotorLargestSetting ? ((
 		}
 	}
 	EXIT_ALL_LOOPS:
-	//cout << "JA,dotarłem tutaj rank " << rank << endl;
+	cout << "JA,dotarłem tutaj rank " << rank << endl;
 	if (rank ==0){
-		MPI_Recv(r,MAX_ROTORS,MPI_INT,MPI_ANY_SOURCE,MPI_ANY_TAG,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		MPI_Recv(r,MAX_ROTORS,MPI_INT,rankr[0],2,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 		dummy[0] = 1;
 		MPI_Ibcast(dummy,1,MPI_INT,0,MPI_COMM_WORLD,&endOfComputing);
-		/*cout << "Wyslalem ibcast " << rank << endl;
-		cout << "Dostalem coś takiego " << r[1] << endl;
-		cout << "Dostalem coś takiego " << r[2] << endl;
-		cout << "Dostalem coś takiego " << r[3] << endl;
-		cout << "Dostalem coś takiego " << r[4] << endl;
-		cout << "Dostalem coś takiego " << r[5] << endl;
-		cout << "Dostalem coś takiego " << r[6] << endl;
-		cout << "Dostalem coś takiego " << r[7] << endl;
-		cout << "Dostalem coś takiego " << r[8] << endl;
-		cout << "Dostalem coś takiego " << r[9] << endl;
-		*/
+		cout << "Wyslalem ibcast " << rank << endl;
+		
 		for ( uint rotor = 0; rotor < rotors; rotor++ ) {
 			this->rotorPositions[ rotor ] = r[ rotor ];
 		}
